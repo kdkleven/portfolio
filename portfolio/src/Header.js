f1277fa3acf94b0141898a94f1b1a54b89ed41f5
Kyle Kleven <kylekleven8@gmail.com>
2021-05-04 23:00:26 -0500

    Created react structure

---
+++ portfolio/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand">Kyle Kleven</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active bg-light" aria-current="page" href="#" id="about">About</a>
+                    <a class="nav-link bg-light" href="#" id="portfolio">Portfolio</a>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
