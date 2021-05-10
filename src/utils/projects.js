bbd44c8299647136c75d9041f41ca41bef98e41f
Kyle Kleven <kylekleven8@gmail.com>
2021-05-09 23:42:58 -0500

    Converted projects into object format, created sample project component

---
+++ src/utils/projects.js
@@ -0,0 +1,46 @@
+var projects = [
+    {
+        "name": "Daily Recreational",
+        "description": "An applicaiton to inspire you to get out of the house and rekindle your love for outdoor activities. Click on the image to try it out!",
+        "image": "./assets/images/DailyRecreational.png",
+        "application": "https://kdkleven.github.io/dailyRecreational/",
+        "repository": "https://github.com/kdkleven/dailyRecreational"
+    },
+    {
+        "name": "Cup of Sugar",
+        "description": "A platform for good neighbors to be good neighbors. Cup of Sugar users can easily find local and national resources, browse posts about what resources or services are available or needed in their community, and connect with one another to help fulfill those needs. Users can create posts of their own as well where they can offer up free food or clothing, or to ask for help with something in their home.",
+        "image": "./assets/images/cupofsugar.png",
+        "application": "https://cup-of-sugar-application.herokuapp.com/",
+        "repository": "https://github.com/kdkleven/Cup-of-Sugar"
+    },
+    {
+        "name": "Tech Blog",
+        "description": "An applicaiton to inspire you to get out of the house and rekindle your love for outdoor activities. Click on the image to try it out!",
+        "image": "./assets/images/techblog.PNG",
+        "application": "https://kk-tech-blog.herokuapp.com/",
+        "repository": "https://github.com/kdkleven/techBlog"
+    },
+    {
+        "name": "iWorkout",
+        "description": "Workout tracker to help you track your daily workout routine.",
+        "image": "",
+        "application": "https://kk-workout.herokuapp.com/",
+        "repository": "https://github.com/kdkleven/iWorkout"
+    },
+    {
+        "name": "budge-it",
+        "description": "Budge-it tracker is a progressive web application that allows you to record transactions while online using MongoDB or offline using indexedDB. Transaction data recorded offline will sync with the online database as soon as a network connection is re-established.",
+        "image": "",
+        "application": "https://kk-budge-it.herokuapp.com/",
+        "repository": "https://github.com/kdkleven/budge-it"
+    },
+    {
+        "name": "empDirStat",
+        "description": "An employee directory, with filter and sort functionality, built using React.",
+        "image": "",
+        "application": "https://kdkleven.github.io/empDirStat/",
+        "repository": "https://github.com/kdkleven/empDirStat"
+    }
+]
+
+export default projects;
